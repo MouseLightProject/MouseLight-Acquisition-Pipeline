9402be862e94510cd7ff5d2b13e262b5261d478b
Patrick Edson <pedson@users.noreply.github.com>
2017-11-15 08:22:14 -0500

    Update configuration to remove api server.

--- options-template.sh
+++ options-template.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
-# Set to a host path that will be mapped to "/opt/pipeline" in the coordinator API container.  This can provide a
-# convenient root mount point from the host for task repositories and data locations.
-export PIPELINE_VOLUME=
-
-# Set to "development" to trigger local development seed data rather than production
-export PIPELINE_SEED_ENV=
+# Location of the the coordinator API server.
+export PIPELINE_API_HOST=
--- up.sh
+++ up.sh
@@ -4,8 +4,8 @@ if [ -a "options.sh" ]; then
     source "options.sh"
 fi
 
-if [ -z "$PIPELINE_VOLUME" ]; then
-    echo "PIPELINE_VOLUME is a required environment variable.  This is typically defined in options.sh"
+if [ -z "PIPELINE_API_HOST" ]; then
+    echo "PIPELINE_API_HOST is a required environment variable.  This is typically defined in options.sh"
     exit 1
 fi
 
