393e0aa49c2dd13c138b9838d8886bf583832d93
Patrick Edson <pedson@users.noreply.github.com>
2018-06-26 12:16:27 -0400

    stop and down perform full services.

--- stop.sh
+++ stop.sh
@@ -8,4 +8,4 @@ if [ -z "PIPELINE_COMPOSE_PROJECT" ]; then
     export PIPELINE_COMPOSE_PROJECT="pipeline"
 fi
 
-docker-compose -p ${PIPELINE_COMPOSE_PROJECT} stop
+docker-compose -f docker-compose.yml -f docker-compose.coordinator.yml -f docker-compose.workers.yml -p ${PIPELINE_COMPOSE_PROJECT} stop
